800cacbf388d2b94a9d67132a39893987ab6c7a4
Martín <martin.ruiz8811@gmail.com>
2017-10-17 20:28:30 -0500

    MainPageAdapter working now

--- app/src/main/java/com/example/martinruiz/snapnotes/adapters/MainPageAdapter.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/adapters/MainPageAdapter.java
@@ -4,11 +4,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.example.martinruiz.snapnotes.fragments.CameraFragment;
+import com.example.martinruiz.snapnotes.fragments.GalleryFragment;
+import com.example.martinruiz.snapnotes.fragments.ProfileFragment;
+
 /**
  * Created by MartinRuiz on 10/17/2017.
  */
 
 public class MainPageAdapter extends FragmentPagerAdapter {
+    private static int NUM_ITEMS = 3;
+
 
     public MainPageAdapter(FragmentManager fm) {
         super(fm);
@@ -16,11 +22,19 @@ public class MainPageAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return GalleryFragment.create();
+            case 1:
+                return CameraFragment.create();
+            case 2:
+                return ProfileFragment.create();
+        }
         return null;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return NUM_ITEMS;
     }
 }
