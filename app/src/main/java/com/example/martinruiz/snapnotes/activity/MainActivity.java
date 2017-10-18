654ed5eb8e05850ab9d250db3166ab7706e0d3f1
Martín <martin.ruiz8811@gmail.com>
2017-10-17 20:28:08 -0500

    Refactoring

--- app/src/main/java/com/example/martinruiz/snapnotes/activity/MainActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/activity/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.martinruiz.snapnotes;
+package com.example.martinruiz.snapnotes.activity;
 
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.martinruiz.snapnotes.R;
+import com.example.martinruiz.snapnotes.adapters.MainPageAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,13 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.ma_background_view) View backgroundView;
     @BindView(R.id.ma_view_pager) ViewPager viewPager;
 
+    private MainPageAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        //viewPager.setAdapter();
+        adapter = new MainPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
     }
 
 }
