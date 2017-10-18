7f66d4e08731501a978cf210c111f21060106b1c
Martín <martin.ruiz8811@gmail.com>
2017-10-17 19:40:25 -0500

    Added ViewBackground and Viewpager to

--- app/src/main/java/com/example/martinruiz/snapnotes/MainActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.martinruiz.snapnotes;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ma_background_view) View backgroundView;
+    @BindView(R.id.ma_view_pager) ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //viewPager.setAdapter();
     }
+
 }
