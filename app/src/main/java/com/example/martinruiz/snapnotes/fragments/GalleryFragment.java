dac9c3548e5e866e8f597076145d70a20eac2779
Martín <martin.ruiz8811@gmail.com>
2017-10-17 19:42:23 -0500

    Addad some fragments and BaseFragment to avoid boilerplait on all fragments.

---
+++ app/src/main/java/com/example/martinruiz/snapnotes/fragments/GalleryFragment.java
@@ -0,0 +1,29 @@
+package com.example.martinruiz.snapnotes.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.martinruiz.snapnotes.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GalleryFragment extends Fragment {
+
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+
+    }
+}
