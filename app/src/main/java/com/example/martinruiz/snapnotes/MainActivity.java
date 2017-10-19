7615854f1c602c71b45ed6a92e081e434df01814
Erik <erikmartin19@gmail.com>
2017-10-19 15:41:29 -0500

    Delete testing code

--- app/src/main/java/com/example/martinruiz/snapnotes/MainActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.martinruiz.snapnotes;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -19,9 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,LogInActivity.class);
-        startActivity(intent);
-        //viewPager.setAdapter();
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(this, LogInActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
