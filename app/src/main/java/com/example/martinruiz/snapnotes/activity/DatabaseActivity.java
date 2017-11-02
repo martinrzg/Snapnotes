d5bcd0923f21a0a87ff58d4916c8ec782ba0b71b
Erik <erikmartin19@gmail.com>
2017-11-01 22:51:45 -0600

    Add Examples to get data

--- app/src/main/java/com/example/martinruiz/snapnotes/activity/DatabaseActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/activity/DatabaseActivity.java
@@ -17,12 +17,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    private static final String TAG = DatabaseActivity.class.getSimpleName();
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private DatabaseReference mNotes;
@@ -37,7 +39,7 @@ public class DatabaseActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        DatabaseCRUD.writeNewNote(mDatabase.child(mAuth.getUid()),new Notes("sdf","fd","34"), "-Kxu9MsTs8L5u9Clf-va");
+        DatabaseCRUD.writeNewNote(mDatabase.child(mAuth.getUid()),new Notes("wwww","ffasdfwverfd","34fsaf"), "-Kxu9MsTs8L5u9Clf-va");
         //DatabaseCRUD.writeNewBoard(mDatabase.child(mAuth.getUid()),new BoardContent("Base de datos", new HashMap<String, Object>()));
 
 
@@ -47,5 +49,43 @@ public class DatabaseActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+
+        //Example get All elements and add a listener
+       /* mDatabase.child(mAuth.getUid()).child("boards").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.e("Count ", "" + snapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    BoardContent boards = postSnapshot.getValue(BoardContent.class);
+                    Log.e("Get Data", boards.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError firebaseError) {
+                Log.e("The read failed: ", "Fail");
+
+
+            }
+        });*/
+
+       //Example to get the notes from a board
+        /*mDatabase.child(mAuth.getUid()).child("boards").child("-Kxu9MsTs8L5u9Clf-va").child("notes").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.e("Count ", "" + snapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    Notes notes = postSnapshot.getValue(Notes.class);
+                    Log.e("Get Data", notes.getId());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError firebaseError) {
+                Log.e("The read failed: ", "Fail");
+
+
+            }
+        });*/
     }
 }
