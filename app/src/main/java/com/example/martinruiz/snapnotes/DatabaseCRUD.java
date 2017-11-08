9934f78591cee354d9fbe961f9f2b5613d642fc0
Erik <erikmartin19@gmail.com>
2017-11-08 15:06:03 -0600

    Create calendar

--- app/src/main/java/com/example/martinruiz/snapnotes/DatabaseCRUD.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/DatabaseCRUD.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.example.martinruiz.snapnotes.DatabaseModel.Boards;
 import com.example.martinruiz.snapnotes.DatabaseModel.BoardContent;
 import com.example.martinruiz.snapnotes.DatabaseModel.Notes;
+import com.example.martinruiz.snapnotes.DatabaseModel.UserCalendar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,13 +25,16 @@ import static android.content.ContentValues.TAG;
 public class DatabaseCRUD {
 
     private static final String BOARDS = "boards";
+    private static final String CALENDAR = "calendar";
+
     /**
-     *Add new note to the selected board.
+     * Add new note to the selected board.
+     *
      * @param mDatabase Database reference to the user database, recommended use: mDatabase.child(mAuth.getUid().
-     * @param note Note object that is going to be add to the notes.
-     * @param boardId Id of the board where the note will be added.
+     * @param note      Note object that is going to be add to the notes.
+     * @param boardId   Id of the board where the note will be added.
      */
-    public static void writeNewNote(final DatabaseReference mDatabase, final Notes note, final String boardId){
+    public static void writeNewNote(final DatabaseReference mDatabase, final Notes note, final String boardId) {
         mDatabase.child("boards").child(boardId).addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
@@ -61,14 +65,14 @@ public class DatabaseCRUD {
         //Get the key for the new note
         String key = mDatabase.child("boards").child(boardId).push().getKey();
 
-        if(boardContent.getNotes() == null){
+        if (boardContent.getNotes() == null) {
 
             note.setId(key);                                                      //Add the id to the note object.
             Map<String, Object> notes = new HashMap();                            //If the notes is empty initialize the HashMap for the notes.
             notes.put(key, note);                                                 //Add th new note to the HashMap
             boardContent.setNotes(notes);                                         //Add the notes HashMap to the boardContent
             mDatabase.child("boards").child(boardId).setValue(boardContent);      //Update the data of the database
-        }else{
+        } else {
             note.setId(key);                                                      //Add the id to the note object.
             boardContent.getNotes().put(key, note);                               //Add th new note to the HashMap
             mDatabase.child(BOARDS).child(boardId).setValue(boardContent);      //Update the data of the database
@@ -79,7 +83,8 @@ public class DatabaseCRUD {
 
     /**
      * Method to add new Board to the user board.
-     * @param mDatabase Database reference to the user database, recommended use: mDatabase.child(mAuth.getUid().
+     *
+     * @param mDatabase    Database reference to the user database, recommended use: mDatabase.child(mAuth.getUid().
      * @param boardContent Object BoardContent that is going to be add to the database.
      */
     public static void writeNewBoard(final DatabaseReference mDatabase, final BoardContent boardContent) {
@@ -118,7 +123,6 @@ public class DatabaseCRUD {
                 });
 
 
-
     }
 
     private static void newBoard(DatabaseReference mDatabase, Map<String, Object> boards, BoardContent boardContent) {
@@ -130,5 +134,62 @@ public class DatabaseCRUD {
         boards.put(key, boardContent);
 
         mDatabase.child(BOARDS).setValue(boards);
+    }
+
+    /**
+     * Method to add new Board to the user board.
+     *
+     * @param mDatabase    Database reference to the user database, recommended use: mDatabase.child(mAuth.getUid().
+     * @param userCalendar Object USerCalendar that is going to be add to the database.
+     */
+    public static void writeNewCalendar(final DatabaseReference mDatabase, final UserCalendar userCalendar) {
+
+
+        mDatabase.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get board value
+                        if (dataSnapshot.hasChild(CALENDAR)) {
+
+                            Boards board = dataSnapshot.getValue(Boards.class);
+
+                            // [START_EXCLUDE]
+                            if (board == null) {
+
+                                // Board is null, error out
+                                Log.e(TAG, "Boards is unexpectedly null");
+                            } else {
+                                // Write new Board
+                                Log.d(TAG,"holi2");
+                                newCalendar(mDatabase, board.getCalendar(), userCalendar);
+
+                            }
+                        } else {
+                            //Initialize boards if is empty
+                            Log.d(TAG,"holi");
+                            Map<String, UserCalendar> map = new HashMap<>();
+                            newCalendar(mDatabase, map, userCalendar);
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getPost:onCancelled", databaseError.toException());
+
+                    }
+                });
+
+
+    }
+
+    private static void newCalendar(DatabaseReference mDatabase, Map<String, UserCalendar> calendars, UserCalendar userCalendar) {
+
+
+        //Insert the new board to the boards HashMap
+        calendars.put(userCalendar.getName(), userCalendar);
+
+        mDatabase.child(CALENDAR).setValue(calendars);
     }
 }
--- app/src/main/java/com/example/martinruiz/snapnotes/DatabaseModel/Boards.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/DatabaseModel/Boards.java
@@ -11,10 +11,16 @@ public class Boards implements Serializable{
 
 
     public Map<String, Object> boards;
-
+    public Map<String, UserCalendar> calendar;
 
     public Boards() {
 
+    }
+
+
+    public Boards(Map<String, Object> boards, Map<String, UserCalendar> calendar) {
+        this.boards = boards;
+        this.calendar = calendar;
     }
 
     public Boards(Map<String, Object> boards) {
@@ -27,5 +33,13 @@ public class Boards implements Serializable{
 
     public void setBoards(Map<String, Object> boards) {
         this.boards = boards;
+    }
+
+    public Map<String, UserCalendar> getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Map<String, UserCalendar> calendar) {
+        this.calendar = calendar;
     }
 }
