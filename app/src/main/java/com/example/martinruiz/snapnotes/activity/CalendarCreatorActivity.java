89039ee1546f8b8f2dc52f9ad369db87009c8fb2
IsaacSec <isaacsecundino3@gmail.com>
2017-11-14 22:41:17 +0000

    CalendarCreator - Simple Course add

--- app/src/main/java/com/example/martinruiz/snapnotes/activity/CalendarCreatorActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/activity/CalendarCreatorActivity.java
@@ -1,13 +1,21 @@
 package com.example.martinruiz.snapnotes.activity;
 
+import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.martinruiz.snapnotes.R;
 import com.example.martinruiz.snapnotes.util.DisplayTool;
+
+import java.util.zip.Inflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,14 +31,39 @@ public class CalendarCreatorActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        TextView test = new TextView(this);
-        test.setText("Holi");
-        ConstraintSet constraints = new ConstraintSet();
-
-        mondayCol.addView(test);
-
-        constraints.clone(mondayCol);
-        constraints.connect(test.getId(), ConstraintSet.TOP, mondayCol.getId(), ConstraintSet.TOP, DisplayTool.convertDpToPixel(60,this));
-        constraints.applyTo(mondayCol);
+        addNewCourse(mondayCol);
     }
+
+    private void addNewCourse(ConstraintLayout weekDay){
+        int width = DisplayTool.convertDpToPixel(100, this);
+        int height = DisplayTool.convertDpToPixel(50, this);
+        int margin =  DisplayTool.convertDpToPixel(42,this);
+
+        View courseCell = createCell(width, height, "Nueva materia en esta semana carnal");
+        ConstraintSet constraints = new ConstraintSet();
+        weekDay.addView(courseCell);
+
+        constraints.clone(weekDay);
+        constraints.connect(courseCell.getId(), ConstraintSet.TOP, weekDay.getId(), ConstraintSet.TOP, margin);
+        constraints.applyTo(weekDay);
+    }
+
+    private View createCell(int width, int height, String course){
+        // Cell width - 100
+        // Cell height - 50
+
+        // Create Layout from xml file
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayoutCompat cell = (LinearLayoutCompat) inflater.inflate(R.layout.calendar_cell, null);
+
+        // Set content in cell
+        TextView content = (TextView) cell.findViewById(R.id.cell_content);
+        content.setText(course);
+
+        // Set cell dimensions
+        cell.setLayoutParams(new LinearLayout.LayoutParams(width,height));
+
+        return cell;
+    }
+
 }
