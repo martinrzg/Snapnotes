501fa73c7673ea934ee1d2fae7e23af3f51f2ada
Erik <erikmartin19@gmail.com>
2017-11-14 21:01:54 -0600

    App flow improvement.

--- app/src/main/java/com/example/martinruiz/snapnotes/activity/SplashActivity.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/activity/SplashActivity.java
@@ -1,27 +1,40 @@
 package com.example.martinruiz.snapnotes.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private SharedPreferences prefs ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //TODO: Comment to start the activity you want for debug.
+
         mAuth = FirebaseAuth.getInstance();
         //FirebaseAuth.getInstance().signOut();
         if(mAuth.getCurrentUser() == null){
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, LogInActivity.class);
             startActivity(intent);
+//            Log.d("Splash","login");
         }else{
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            prefs = this.getSharedPreferences("com.example.martinruiz.snapnotes."+mAuth.getCurrentUser().getUid(), Context.MODE_PRIVATE);
+            if(!prefs.getBoolean("calendar",false)) {
+                Intent intent = new Intent(this, CalendarActivity.class);
+                startActivity(intent);
+//                Log.d("Splash","calendar");
+            }else {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+//                Log.d("Splash","main");
+            }
         }
         finish();
     }
