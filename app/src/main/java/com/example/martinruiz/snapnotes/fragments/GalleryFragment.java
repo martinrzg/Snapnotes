5f3b93ec2882bd7f71cff6d110a26a65afa4320b
Erik <erikmartin19@gmail.com>
2017-11-23 11:13:12 -0600

    Add icon

--- app/src/main/java/com/example/martinruiz/snapnotes/fragments/GalleryFragment.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/fragments/GalleryFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -297,4 +298,5 @@ public class GalleryFragment extends Fragment {
                     }
                 });
     }
+
 }
