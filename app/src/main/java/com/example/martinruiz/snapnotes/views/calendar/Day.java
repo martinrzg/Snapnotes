fdad42cf68ff0bb935d75113ba342fbd4defb951
IsaacSec <isaacsecundino3@gmail.com>
2017-11-29 20:54:41 +0000

    Course model - toString changed to three words format

--- app/src/main/java/com/example/martinruiz/snapnotes/views/calendar/Day.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/views/calendar/Day.java
@@ -55,13 +55,13 @@ public enum Day{
     @Override
     public String toString(){
         switch (this){
-            case MONDAY: return "Monday";
-            case TUESDAY: return "Tuesday";
-            case WEDNESDAY: return "Wednesday";
-            case THURSDAY: return "Thursday";
-            case FRIDAY: return "Friday";
-            case SATURDAY: return "Saturday";
-            case SUNDAY: return "Sunday";
+            case MONDAY: return "Mon";
+            case TUESDAY: return "Tue";
+            case WEDNESDAY: return "Wed";
+            case THURSDAY: return "Thu";
+            case FRIDAY: return "Fri";
+            case SATURDAY: return "Sat";
+            case SUNDAY: return "Sun";
             default: return "null";
         }
     }
