a97fe667e1f4fb23499455fe5aee209d92904517
Martín Ruiz <martin.ruiz8811@gmail.com>
2017-11-30 12:53:47 -0600

    Add
    - Settings activity and layout

--- app/src/main/java/com/example/martinruiz/snapnotes/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/martinruiz/snapnotes/fragments/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.example.martinruiz.snapnotes.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.martinruiz.snapnotes.R;
+import com.example.martinruiz.snapnotes.activity.LogInActivity;
+import com.example.martinruiz.snapnotes.activity.SettingsActivity;
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -41,7 +48,18 @@ public class ProfileFragment extends Fragment {
 
     @OnClick(R.id.imageViewSettings)
     public void settingsClick(){
-        
+        Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
+        startActivity(settingsIntent);
+        /*AuthUI.getInstance().signOut(getActivity())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent loginIntent = new Intent(getActivity(), LogInActivity.class);
+                        startActivity(loginIntent);
+                        getActivity().finish();
+                    }
+                });
+        */
     }
 
 }
